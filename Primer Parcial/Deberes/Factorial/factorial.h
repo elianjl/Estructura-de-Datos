4885d8dd1fc10f85b54bfad8043fb3b7912d5322
elianjl <55764250+elianjl@users.noreply.github.com>
2019-10-15 13:17:15 -0500

    Recursividad Factorial

---
+++ Primer Parcial/Deberes/Factorial/factorial.h
@@ -0,0 +1,57 @@
+/*
+Universidad de las Fuerzas Armadas ESPE
+Nombre: Elian Llorente
+NRC:2967
+Fecha de Creacion: 23/09/2019
+Fecha de Modificacion: 15/10/2019
+*/
+
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+class Dato{
+	private:
+	int valor;
+	public:
+	Dato();
+	Dato(int);
+	void setValor(int);
+	int getValor();
+	void toString();
+	int procesoF(int);	
+	
+};
+
+Dato::Dato(){
+	setValor(0);
+}
+
+Dato::Dato(int valorNuevo){
+	setValor(valorNuevo);
+}
+
+void Dato::setValor(int valorNuevo){
+	valor = valorNuevo;
+}
+
+int Dato::getValor(){
+	return valor;
+}
+
+void Dato::toString(){
+	cout<<"Valor:"<<valor<<endl;
+}
+
+int Dato::procesoF(int fact){
+	
+	if(fact == 1){
+		return 1;	
+	}
+	else{
+		return fact*procesoF(fact-1);
+	}	
+}
+
+
---
+++ Primer Parcial/Deberes/Factorial/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "factorial.h"
+
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
